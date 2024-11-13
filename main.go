3aa3182ea1689f973aee2f36d8d5b81420b14e32
Ryan Cua <ry.j.cua@gmail.com>
2024-11-12 19:45:19 -0800

    add metrics

--- main.go
+++ main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"net/http"
+	"log"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 
 func main() {
 	newServerMux := http.NewServeMux()
-	var server http.Server
+	const filepathRoot = "."
+	const adminPath = "./admin"
 
-	server.Handler = newServerMux
-	server.Addr = ":8080"
-	newServerMux.Handle("/", http.FileServer(http.Dir(".")))
-	newServerMux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	const api_prefix = "/api"
+	const admin_prefix = "/admin"
+	const port = "8080"
+	server :=  &http.Server{
+		Addr: ":" + port,
+		Handler: newServerMux,
+	}
+	apiConfigVar:= apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-	server.ListenAndServe()
+	newServerMux.Handle("/app/", apiConfigVar.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
+	newServerMux.Handle("/admin/", apiConfigVar.middlewareMetricsInc(http.StripPrefix("/admin", http.FileServer(http.Dir(adminPath)))))
+	newServerMux.HandleFunc(fmt.Sprintf("GET %v/healthz", api_prefix), handlerReadiness)	
+	newServerMux.HandleFunc(fmt.Sprintf("GET %v/metrics", admin_prefix), apiConfigVar.handlerMetrics)
+	newServerMux.HandleFunc(fmt.Sprintf("POST %v/reset", admin_prefix), apiConfigVar.handlerReset)
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
+
---
+++ readiness.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"net/http"
+)
+
+func handlerReadiness(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
+}
